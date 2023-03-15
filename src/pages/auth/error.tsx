b1170bcdbd8532f122e0e05f2c4c00409e71993d
Zack F <zackaryfotheringham@hotmail.com>
2023-03-14 22:31:07 -0400

    setup nextAuth with google and facebook

---
+++ src/pages/auth/error.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from "next";
+
+const ErrorPage: NextPage = () => {
+  return (
+    <div>
+      error
+      <div></div>
+    </div>
+  );
+};
+
+export default ErrorPage;

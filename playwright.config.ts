94b6c5cc41f5d88d372477f1cb5e5dc0a4b1332b
Zack F <zackaryfotheringham@hotmail.com>
2023-04-05 00:06:48 -0400

    fix webkit not being loaded

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     trace: "on-first-retry",
 
     /* Run browser in headless mode */
-    headless: process.env.CI ? true : false,
+    headless: true,
   },
 
   /* Configure projects for major browsers */

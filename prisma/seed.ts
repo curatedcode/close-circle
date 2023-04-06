079b3dc375ecd2e88b41097894612fb74788a6e0
Zack F <zackaryfotheringham@hotmail.com>
2023-04-06 01:44:20 -0400

    fix prisma seed not working on new database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,232 +2,201 @@ import { faker } from "@faker-js/faker";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-const USERS_TO_CREATE = 20;
+const USERS = 50;
 
-// posts per user
-const USER_POSTS_MIN = 1;
-const USER_POSTS_MAX = 10;
+// min/max posts per user
+const POSTS_MIN = 1;
+const POSTS_MAX = 10;
 
-// likes per post
-const POST_LIKE_MIN = 1;
-const POST_LIKE_MAX = 5;
+// min/max likes per post
+const LIKE_MIN = 1;
+const LIKE_MAX = 15;
 
 // comments per post
 const COMMENT_MIN = 2;
 const COMMENT_MAX = 20;
 
 // images per post
-const POST_IMAGE_MIN = 0;
-const POST_IMAGE_MAX = 5;
-
-type Id = {
-  connect: {
-    id: string;
-  };
-};
+const IMAGE_MIN = 0;
+const IMAGE_MAX = 5;
 
 type Post = {
-  user: Id;
+  userId: string;
   body: string;
   createdAt: Date;
 };
 
 type PostLike = {
-  post: Id;
-  user: Id;
+  postId: string;
+  userId: string;
 };
 
 type Comment = {
   body: string;
-  post: Id;
-  user: Id;
+  postId: string;
+  userId: string;
   createdAt: Date;
 };
 
 type PostImage = {
   url: string;
-  post: Id;
+  postId: string;
 };
 
+const userData = Array.from({ length: USERS }).map(() => ({
+  name: faker.name.fullName(),
+  email: faker.internet.email(),
+  image: faker.internet.avatar(),
+  bio: faker.lorem.sentence(),
+}));
+
+function getRandomPostBody(): string {
+  const body = faker.lorem.paragraph(1);
+  // posts can't be longer than 280 characters
+  if (body.length > 280) return getRandomPostBody();
+  return body;
+}
+
+function getRandomDate() {
+  const today = new Date().toISOString();
+  return faker.date.between("2022-01-01T00:00:00.000Z", today);
+}
+
+function getRandomCommentBody() {
+  // randomly return 3 words or an emoji
+  const randomNum = Math.abs(Math.floor(Math.random() * 3));
+
+  if (randomNum > 1) return faker.internet.emoji();
+  return faker.lorem.words(3);
+}
+
+function getRandomPhotoCategory() {
+  const categories = [
+    "dog",
+    "car",
+    "house",
+    "paris",
+    "brazil",
+    "ocean",
+    "boat",
+    "building",
+    "travel",
+    "football",
+  ];
+  const randomNum = Math.abs(Math.floor(Math.random() * 10));
+
+  return categories[randomNum];
+}
+
 async function run() {
-  const userData = Array(USERS_TO_CREATE)
-    .fill(null)
-    .map(() => {
-      return {
-        name: faker.name.fullName(),
-        email: faker.internet.email(),
-        image: faker.image.avatar(),
-        bio: faker.lorem.sentence(),
-      };
-    });
+  // create random users
 
-  const createUsers = userData.map((user) =>
-    prisma.user.create({ data: user })
-  );
-  const users = await prisma.$transaction(createUsers);
+  await prisma.user.createMany({ data: userData });
 
-  const postsArr: Post[] = [];
+  const users = await prisma.user.findMany({ select: { id: true } });
+
+  function getRandomUserId(userIndex: number): string {
+    const user = users[userIndex];
+
+    if (!user) return getRandomUserId(userIndex - 1);
+
+    return user.id;
+  }
+
+  // create random amount of posts for each user
+  const postData: Post[] = [];
 
   users.forEach((user) => {
     const postAmount = faker.datatype.number({
-      min: USER_POSTS_MIN,
-      max: USER_POSTS_MAX,
+      min: POSTS_MIN,
+      max: POSTS_MAX,
     });
 
-    function getPostBody(body: string): string {
-      // posts can't be longer than 280 characters
-      if (body.length > 280) return getPostBody(faker.lorem.paragraph(1));
-      return body;
-    }
-
-    for (let b = 0; b < postAmount; b++) {
-      const date = faker.date.between(
-        "2023-01-01T00:00:00.000Z",
-        "2023-03-023T00:00:00.000Z"
-      );
-
-      postsArr.push({
-        user: {
-          connect: {
-            id: user.id,
-          },
-        },
-        body: getPostBody(faker.lorem.paragraph(1)),
-        createdAt: date,
+    for (let i = 0; i < postAmount; i++) {
+      postData.push({
+        userId: user.id,
+        body: getRandomPostBody(),
+        createdAt: getRandomDate(),
       });
     }
   });
 
-  const createPosts = postsArr.map((post) =>
-    prisma.post.create({ data: post })
-  );
-  const posts = await prisma.$transaction(createPosts);
+  await prisma.post.createMany({ data: postData });
 
-  const postLikesArr: PostLike[] = [];
-  const commentsArr: Comment[] = [];
-  const postImagesArr: PostImage[] = [];
+  const posts = await prisma.post.findMany({ select: { id: true } });
 
-  posts.forEach((post, index) => {
+  // create random amount of likes for each post
+  const postLikeData: PostLike[] = [];
+
+  posts.forEach((post) => {
     const likeAmount = faker.datatype.number({
-      min: POST_LIKE_MIN,
-      max: POST_LIKE_MAX,
+      min: LIKE_MIN,
+      max: LIKE_MAX,
     });
-    for (let b = 0; b < likeAmount; b++) {
-      const user = users[b] ? users[b]?.id : users[0]?.id;
-      postLikesArr.push({
-        post: {
-          connect: {
-            id: post.id,
-          },
-        },
-        user: {
-          connect: {
-            id: user ?? "",
-          },
-        },
+
+    for (let i = 0; i < likeAmount; i++) {
+      postLikeData.push({
+        postId: post.id,
+        userId: getRandomUserId(i),
       });
     }
+  });
 
+  await prisma.postLike.createMany({ data: postLikeData });
+
+  // create random amount of comments for each post
+  const commentData: Comment[] = [];
+
+  posts.forEach((post) => {
     const commentAmount = faker.datatype.number({
       min: COMMENT_MIN,
       max: COMMENT_MAX,
     });
-    // alternate between emojis and words
-    function randomBody() {
-      const randomNum = Math.abs(Math.floor(Math.random() * 3));
 
-      if (randomNum > 1) return faker.internet.emoji();
-      return faker.lorem.words(3);
-    }
-
-    for (let b = 0; b < commentAmount; b++) {
-      const user = users[b] ? users[b]?.id : users[0]?.id;
-      const date = faker.date.between(
-        "2022-05-01T00:00:00.000Z",
-        "2023-03-016T00:00:00.000Z"
-      );
-      commentsArr.push({
-        body: randomBody(),
-        post: {
-          connect: {
-            id: post.id,
-          },
-        },
-        user: {
-          connect: {
-            id: user ?? "",
-          },
-        },
-        createdAt: date,
+    for (let i = 0; i < commentAmount; i++) {
+      commentData.push({
+        postId: post.id,
+        userId: getRandomUserId(i),
+        body: getRandomCommentBody(),
+        createdAt: getRandomDate(),
       });
     }
+  });
 
-    function getRandomPostId(max: number): string | void {
-      const randomNum = Math.abs(Math.floor(Math.random() * (max - 1)));
+  await prisma.comment.createMany({ data: commentData });
 
-      if (posts[randomNum]?.id) return posts[randomNum]?.id ?? "";
+  // create random images for some posts
 
-      return getRandomPostId(max);
-    }
+  const postImageData: PostImage[] = [];
 
-    function getRandomPhotoCategory() {
-      const categories = [
-        "dog",
-        "car",
-        "house",
-        "paris",
-        "brazil",
-        "ocean",
-        "boat",
-        "building",
-        "travel",
-        "football",
-      ];
-      const randomNum = Math.abs(Math.floor(Math.random() * 10));
-
-      return categories[randomNum];
-    }
-
-    const postImageAmount = faker.datatype.number({
-      min: POST_IMAGE_MIN,
-      max: POST_IMAGE_MAX,
+  posts.forEach((post, index) => {
+    const imageAmount = faker.datatype.number({
+      min: IMAGE_MIN,
+      max: IMAGE_MAX,
     });
-    // helps stop every post having an image
+
+    // stop every post having an image
     if (index % 2 === 0) {
-      for (let b = 0; b < postImageAmount; b++) {
-        postImagesArr.push({
+      for (let i = 0; i < imageAmount; i++) {
+        postImageData.push({
+          postId: post.id,
           url: faker.image.imageUrl(
             undefined,
             undefined,
             getRandomPhotoCategory(),
             true
           ),
-          post: {
-            connect: {
-              // assign images randomly
-              id: getRandomPostId(200) ?? "",
-            },
-          },
         });
       }
     }
   });
 
-  const createPostLikes = postLikesArr.map((like) =>
-    prisma.postLike.create({ data: like })
-  );
-  await prisma.$transaction(createPostLikes);
+  await prisma.postImage.createMany({ data: postImageData });
 
-  const createComments = commentsArr.map((comment) =>
-    prisma.comment.create({ data: comment })
-  );
-  await prisma.$transaction(createComments);
-
-  const createPostImage = postImagesArr.map((img) =>
-    prisma.postImage.create({ data: img })
-  );
-  await prisma.$transaction(createPostImage);
   await prisma.$disconnect();
 }
 
-void run();
+run().catch((e) => {
+  console.log(e);
+  process.exit(1);
+});
